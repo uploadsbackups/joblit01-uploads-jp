bd795fe0ba0704014d1a250546ab85d49b3ab9a5
upload01 <uploadsbackups@gmail.com>
2024-11-05 00:20:13 +0900

    2 Commit

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
+          UPLOAD_TEST
         </h1>
 
         <p className={styles.description}>
